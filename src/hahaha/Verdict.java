f954250e85b41109c1dafc475f0b29e29bb759d0
350265287 <350265287@C-FTFYJB2.tdsb.on.ca>
2026-05-14 11:13:39 -0400

    jingkang comment

---
+++ src/hahaha/Verdict.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hahaha;
+
+/**
+ *
+ * @author 350265287
+ */
+public class Verdict {
+    private String caseName;
+    private String studentVerdict;
+    private String reason;
+    public Verdict(){
+    
+    }
+    public Verdict(String caseName, String Verdict, String reason){}
+    
+    public void setCaseName(){}
+    public String getClassName(String title){return "hahaha";}
+    public void setReason(){}
+    public String getReason(String reason){return "hahaha";}
+    public void setStudentVerdict(){}
+    public String getStudentVerdict(String verdict){return "hahaha";}
+    public String toString(){return "hahaha";}
+}
